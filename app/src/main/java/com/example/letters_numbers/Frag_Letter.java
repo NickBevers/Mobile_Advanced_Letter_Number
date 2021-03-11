7c2fb051efedb7894cc92da7d7f5b780df8ff6dc
nickbevers <nick.bevers9@gmail.com>
2021-03-11 21:16:26 +0100

    Added letter & number fragment layout files + classes

---
+++ app/src/main/java/com/example/letters_numbers/Frag_Letter.java
@@ -0,0 +1,16 @@
+package com.example.letters_numbers;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Frag_Letter extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.frag_letter, container, false);
+}
---
+++ app/src/main/java/com/example/letters_numbers/Frag_Number.java
@@ -0,0 +1,18 @@
+package com.example.letters_numbers;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class Frag_Number extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.frag_number, container, false);
+    }
+}
